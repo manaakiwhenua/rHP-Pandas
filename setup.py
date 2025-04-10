be98a69f2455d9b05bc308532fae02b7a5ea592c
Richard Law <lawr@landcareresearch.co.nz>
2025-04-10 15:33:11 +1200

    updates dependency for rhealpixdggs to pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from os import path
 
-__version__: str = "0.1.0"
+__version__: str = "0.1.1"
 
 # read the contents of README file
 this_directory = path.abspath(path.dirname(__file__))

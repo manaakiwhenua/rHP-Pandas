b207b0a38a38d60166d448a20a82085532579863
Nicoletta De Maio <demaion@landcareresearch.co.nz>
2025-05-02 16:48:17 +1200

    Decided that the next release should be minor not patch

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from os import path
 
-__version__: str = "0.1.3"
+__version__: str = "0.2.0"
 
 # read the contents of README file
 this_directory = path.abspath(path.dirname(__file__))
